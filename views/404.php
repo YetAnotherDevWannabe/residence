ece6f121e252a39c2dc1b47332b10c4a592934aa
Ashginger <oferriot@gmail.com>
2023-11-08 11:17:25 +0100

    fix: 404: changes to top menu variables + breadcrumb: changed the start of the breadcrumb to dashboard

--- views/404.php
+++ views/404.php
@@ -7,24 +7,23 @@
 
 <body>
 	<?php
-	$elem1 = new stdClass();
-	$elem1->type = 'Apartment';
-	$elem1->name = 'Apart Lyon';
-
-	$elem2 = new stdClass();
-	$elem2->type = 'House';
-	$elem2->name = 'House Paris';
-
-	$residence = $elem1;
-	// $residence = $elem2;
 	if (isConnected()) {
 		$topNavStart = VIEWS_DIR.'partials/navbar/breadcrumb.php';
-		$topNavCenter = '';
+		$topNavCenter = null;
 		$topNavEnd = VIEWS_DIR.'partials/navbar/connected-user.php';
+
+		// Load the $_SESSION user from DB
+		$userManager = new App\Models\Managers\UserManager();
+		$dbUser = $userManager->getOneById($_SESSION['user']->getId());
+	
+		// Load all the Residences for the connected User
+		$residenceManager = new App\Models\Managers\ResidenceManager();
+		$residences = $residenceManager->getAllByUserId($dbUser->getId());
+		$selectedResidence = null;
 	} else {
-		$topNavStart = VIEWS_DIR.'partials/navbar/breadcrumb.php';
-		$topNavCenter = '';
-		$topNavEnd = '';
+		$topNavStart = VIEWS_DIR.'partials/navbar/logo.php';
+		$topNavCenter = null;
+		$topNavEnd = null;
 	}
 	include VIEWS_DIR.'partials/navbar/_top-navbar.php';
 	?>
